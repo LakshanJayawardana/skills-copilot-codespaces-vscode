4edd9823e00b630c228c38546a82125dc330c040
Lakshan Jayawardana <63670833+LakshanJayawardana@users.noreply.github.com>
2024-05-27 18:58:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return {
+    skills: [
+      {
+        name: "JavaScript",
+        level: "70%",
+      },
+      {
+        name: "React",
+        level: "60%",
+      },
+      {
+        name: "NodeJs",
+        level: "50%",
+      },
+    ],
+  };
+}
